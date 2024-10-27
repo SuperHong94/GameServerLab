bfb108a4f54352e270a5fbf74e86782764b4e2cd
SuperHong94 <ghdtnswh213@gmail.com>
2024-10-27 16:15:19 +0900

    [서버] 기본 에코서버 클라이언트 작성

---
+++ echoServer/BaseEchoClient/BaseEchoClient/BaseEchoClient.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>//2는 버전임
+
+using namespace std;
+#pragma comment(lib,"WS2_32.LIB") //표준에없기에 라이브러리도 추가해야됨
+
+const char* SERVER_IP = "127.0.0.1";
+
+const short SERVER_PORT = 4000;
+const int BUFSIZE = 256;
+void err_display(int err_num) {
+   WCHAR* lpMSgbuf;
+   FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+      NULL, err_num, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      (LPWSTR)&lpMSgbuf, 0, 0);
+   wcout << lpMSgbuf << endl;
+   while (true);
+   LocalFree(lpMSgbuf);
+
+}
+int main()
+{
+   wcout.imbue(locale("korean"));
+   WSADATA wsaData;
+   WSAStartup(MAKEWORD(2, 0), &wsaData);//윈도우에서는 호환성때문에 이것을 사용해야한다.
+   SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
+   SOCKADDR_IN server_addr;
+   ZeroMemory(&server_addr, sizeof(server_addr));
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_port = htons(SERVER_PORT);
+   inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
+   connect(s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+
+   for (;;) {
+      char buf[BUFSIZE];
+      cout << "Enter Message : ";
+      cin.getline(buf, BUFSIZE);//cin은 워드단위로 받는것이라서 공백이 들어가면 잘린다. Hello world 한번에 못받으니깐 cin.getline()으로 한줄 받게한다.
+      WSABUF mybuf;
+      mybuf.buf = buf;
+      mybuf.len = static_cast<ULONG>(strlen(buf) + 1);//실제로 보낸 사이즈 보내기 ,strlen(buf)하면 마지막 널문자를 안샌다. 그래서 널까지 보내기위해 +1을한다.
+      DWORD sent_bytes;
+      int ret=WSASend(s_socket, &mybuf, 1, &sent_bytes, 0, 0, 0);
+      if (SOCKET_ERROR == ret)
+      {
+         cout << "Send Error :";
+         int err_num = WSAGetLastError();
+         err_display(err_num);
+      }
+      char recvBuf[BUFSIZE];
+      WSABUF mybuf_r;
+      mybuf_r.buf = recvBuf;
+      mybuf_r.len = BUFSIZE;
+      DWORD recv_bytes;
+      DWORD recv_flag = 0;
+      WSARecv(s_socket, &mybuf_r, 1, &recv_bytes, &recv_flag, 0, 0);
+      cout << "Server Sent: [" << recv_bytes << " bytes] :" << recvBuf<<endl;
+   }
+   closesocket(s_socket);
+   WSACleanup();
+}
---
+++ echoServer/BaseEchoServer/iocp_echo_server/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <WS2tcpip.h>//2는 버전임
+
+using namespace std;
+#pragma comment(lib,"WS2_32.LIB") //표준에없기에 라이브러리도 추가해야됨
+
+const char* SERVER_IP = "127.0.0.1";
+const short SERVER_PORT = 4000;
+const int BUFSIZE = 256;
+
+void InitWinSock()
+{
+   WSADATA wsaData;
+   WSAStartup(MAKEWORD(2, 0), &wsaData);//윈도우에서는 호환성때문에 이것을 사용해야한다.
+}
+
+
+int main()
+{
+   InitWinSock();
+   SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
+   SOCKADDR_IN server_addr;
+   ZeroMemory(&server_addr, sizeof(server_addr));
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_port = htons(SERVER_PORT);
+   server_addr.sin_addr.s_addr = INADDR_ANY;
+   bind(s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+   listen(s_socket, SOMAXCONN);
+
+   INT addr_size = sizeof(server_addr);
+   SOCKET c_socket = WSAAccept(s_socket, reinterpret_cast<sockaddr*>(&server_addr), &addr_size, 0, 0);
+   for (;;)
+   {
+
+      char recvBuf[BUFSIZE];
+      WSABUF mybuf;
+      mybuf.buf = recvBuf;
+      mybuf.len = BUFSIZE;
+      DWORD recv_bytes;
+      DWORD recv_flag = 0;
+      auto result = WSARecv(c_socket, &mybuf, 1, &recv_bytes, &recv_flag, 0, 0);
+	  if (result == SOCKET_ERROR || 0 == recv_bytes)
+	  {
+		  cout << "WSARecv() Error" << endl;
+		  break;
+	  }
+      cout << "Client Sent: [" << recv_bytes << " bytes] :" << recvBuf << endl;
+
+      mybuf.len = recv_bytes;
+      DWORD sent_bytes;
+      WSASend(c_socket, &mybuf, 1, &sent_bytes, 0, 0, 0);
+   }
+   closesocket(s_socket);
+   closesocket(c_socket);
+   WSACleanup();
+}
